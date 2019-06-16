b6285c17d5e4c0f7255a2be4bbb33fd0cccf28ed
Sid200026 <siddharthsingharoy@gmail.com>
2019-06-17 01:46:24 +0530

    Changed index file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createLogger} from 'redux-logger';
+import { Provider } from 'react-redux';
+import { createStore,combineReducers, applyMiddleware } from 'redux';
+import PlayerRegistration from './Components/PlayerRegistration';
+import { Change_Id_Of_Robot } from './Redux_JS_Files/reducers';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({ Change_Id_Of_Robot });
+
+const logger = createLogger();
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+ReactDOM.render(<Provider store={store}>
+                    <PlayerRegistration />
+                </Provider>
+                , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
