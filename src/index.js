6df3aa1d815d3e31732bc19da3ed538e1b11967d
Sid200026 <siddharthsingharoy@gmail.com>
2019-06-17 13:51:28 +0530

    Finished Registration Page

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ import * as serviceWorker from './serviceWorker';
 import {createLogger} from 'redux-logger';
 import { Provider } from 'react-redux';
 import { createStore,combineReducers, applyMiddleware } from 'redux';
-import PlayerRegistration from './Components/PlayerRegistration';
-import { Change_Id_Of_Robot } from './Redux_JS_Files/reducers';
+import App from './Containers/App';
+import { Change_Id_Of_Robot, Get_Player2_Details, Get_Player1_Details } from './Redux_JS_Files/reducers';
 
-const rootReducer = combineReducers({ Change_Id_Of_Robot });
+const rootReducer = combineReducers({ Change_Id_Of_Robot, Get_Player2_Details, Get_Player1_Details });
 
 const logger = createLogger();
 
 const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}>
-                    <PlayerRegistration />
+                    <div style={{height:'100%',width:'100%'}}>
+                        <App />
+                    </div>
                 </Provider>
                 , document.getElementById('root'));
 
