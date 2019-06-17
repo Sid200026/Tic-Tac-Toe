ab4665027229f2b739bd9f41b8af9227cefc9517
Sid200026 <siddharthsingharoy@gmail.com>
2019-06-17 18:51:13 +0530

    Created the Player Details Component

---
+++ src/Components/PlayerProfiles.js
@@ -0,0 +1,34 @@
+import React,{Component} from 'react';
+import { connect } from 'react-redux';
+import '../CSS/PlayerProfiles.css';
+const mapStateToProps = (state) => {
+    return{
+        id1:state.Get_Player1_Details.id,
+        name1:state.Get_Player1_Details.name,
+        wins1:state.Get_Player1_Details.wins,
+        id2:state.Get_Player2_Details.id,
+        name2:state.Get_Player2_Details.name,
+        wins2:state.Get_Player2_Details.wins,
+    }
+}
+
+class PlayerProfiles extends Component{
+    render(){
+        return(
+            <div className='PlayerDetails'>
+                <div className='Player1'>
+                    <h1>{this.props.name1}</h1>
+                    <img src={`https://robohash.org/${this.props.id1}`} alt='Player1'></img>
+                    <h1>Wins : {this.props.wins1}</h1>
+                </div>
+                <div className='Player2'>
+                    <h1>{this.props.name2}</h1>
+                    <img src={`https://robohash.org/${this.props.id2}`} alt='Player2'></img>
+                    <h1 style={{marginBottom:'10px'}}>Wins : {this.props.wins2}</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default connect(mapStateToProps,{})(PlayerProfiles);
