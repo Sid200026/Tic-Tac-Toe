107e9dfbf0c7b99c8296d4296a4eff908f0123f5
Sid200026 <siddharthsingharoy@gmail.com>
2019-06-17 13:53:06 +0530

    Removed some typos

---
+++ src/Containers/App.js
@@ -0,0 +1,44 @@
+import React,{Component} from 'react';
+import PlayerRegistration from '../Components/PlayerRegistration';
+import '../CSS/app.css';
+
+class app extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            playerNumber : 1,
+            PlayerRegistrationComplete : false,
+        };
+    }
+
+    register = () => {
+        if(this.state.playerNumber === 1)
+            this.setState({playerNumber:this.state.playerNumber+1});
+        else   
+            this.setState({PlayerRegistrationComplete:true});
+    }
+
+    render(){
+        return(
+            <div className='background'>
+                <h1 style={{fontSize:'45px',textAlign:'center',marginBottom:'15px',marginTop:'8px'}} className='header'>Tic-Tac-Toe</h1>
+                {this.state.PlayerRegistrationComplete === false &&
+                    <div className='register'>
+                        <PlayerRegistration register={this.register} playerNumber={this.state.playerNumber}/>
+                    </div>
+                }
+                {this.state.PlayerRegistrationComplete === true &&
+                    <h1>Hello World</h1>
+                }
+                <div className='link'>
+                    <a href="https://github.com/Sid200026" className='linkText'target='_blank' rel="noopener noreferrer">
+                        <h4 >Made by Siddharth Singha Roy</h4>
+                    </a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default app;
