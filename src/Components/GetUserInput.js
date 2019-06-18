f30ed94402684d1def3e678283d92a3e1bb0f5e1
Sid200026 <siddharthsingharoy@gmail.com>
2019-06-19 00:06:38 +0530

    Added the condition for checking cross or nuts

--- src/Components/GetUserInput.js
+++ src/Components/GetUserInput.js
@@ -4,7 +4,14 @@ import '../CSS/TicTacToeBox.css';
 class GetUserInput extends Component{
     render(){
         return(
-            <h1 style={{fontSize:'70px'}}>{this.props.text}</h1>
+            <div>
+            {this.props.text === 'X' &&
+                <h1 style={{fontSize:'70px',color:'red'}}>{this.props.text}</h1>
+            }
+            {this.props.text === 'O' &&
+                <h1 style={{fontSize:'70px',color:'blue'}}>{this.props.text}</h1>
+            }
+            </div>
         );
     }
 }
