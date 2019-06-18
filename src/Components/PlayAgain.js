72e11c6247218683961a1c27eb13551fc343282f
Sid200026 <siddharthsingharoy@gmail.com>
2019-06-19 00:08:53 +0530

    Play Again Component Initial Commit

---
+++ src/Components/PlayAgain.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import '../CSS/PlayAgain.css';
+
+class PlayAgain extends Component{
+    render(){
+        return(
+            <div>
+                <button type='button'>Play Again</button>
+            </div>
+        )
+    }
+}
+
+export default PlayAgain;
